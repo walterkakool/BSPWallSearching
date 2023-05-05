134632618adc769c798b419aeb047ca80fe57bf5
Walter Tan <walterthaim@ugls2.ece.uvic.ca>
2023-05-05 16:36:15 -0700

    removed Eigen stuff from .cpp

--- testingsfenv/src/inputoutput.cpp
+++ testingsfenv/src/inputoutput.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
-#include <Eigen/Geometry>
+//#include <Eigen/Geometry>
 #include <vector>
 
 using namespace std;
-using namespace Eigen;
+//using namespace Eigen;
 
 
-void foo(std::vector<int> v) {
+void foo(std::vector<int>& v) {
 	v.pop_back();
 	v.push_back(20);
 }
 
 int main(int argc, char** args) {
 
-	Eigen::MatrixXi facets;
+//	Eigen::MatrixXi facets;
 	for (int i = 0; i < argc; i++)
 		std::cout << args[i] << endl;
 
 	std::vector<int> v;
 	v.push_back(1);
 	foo(v);
-	std::cout << v.at(0) << endl;
+	std::cout << *(v.begin()) <<"\n"<< endl;
 	return 0;
 }
