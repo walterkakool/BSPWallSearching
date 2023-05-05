6ffbde6ccf609ccbd04c9c03cdbf2ff9296dcad5
walkk <72165225+walterkakool@users.noreply.github.com>
2023-05-05 16:24:45 -0700

    Update inputoutput.cpp

--- testingsfenv/src/inputoutput.cpp
+++ testingsfenv/src/inputoutput.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <Eigen/Geometry>
+#include <vector>
 
 using namespace std;
+using namespace Eigen;
+
+
+void foo(std::vector<int> v) {
+	v.pop_back();
+	v.push_back(20);
+}
 
 int main(int argc, char** args) {
+
+	Eigen::MatrixXi facets;
 	for (int i = 0; i < argc; i++)
 		std::cout << args[i] << endl;
+
+	std::vector<int> v;
+	v.push_back(1);
+	foo(v);
+	std::cout << v.at(0) << endl;
 	return 0;
 }
