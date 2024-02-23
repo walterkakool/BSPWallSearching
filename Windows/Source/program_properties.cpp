6b992547aa02a4b47d5ce225843e45ab2136290c
walkk <72165225+walterkakool@users.noreply.github.com>
2024-02-23 10:56:09 -0800

    Implemented func's in program_properties

---
+++ Windows/Source/program_properties.cpp
@@ -0,0 +1,47 @@
+#include "pch.h"
+#include "framework.h"
+#include "bsp_maze.h"
+
+#include <iostream>
+#include <queue>
+#include <stdio.h>
+#include <iomanip>
+#include <typeinfo>
+#include <assert.h>   
+#include <list>
+#include <string>
+#include <cmath>
+#include <iterator>
+#include <cstddef>
+#include <array>
+#include <math.h>
+#include <type_traits>
+#include <cstdint>
+#include <boost/intrusive/rbtree.hpp>
+#include <boost/multi_array.hpp>
+#include <exception>
+#include <typeinfo>
+#include <functional>
+#include <stdexcept>
+#include <memory>
+#include <algorithm>  
+#include <utility>
+#include <set>
+#include <numbers>
+#include <complex>
+#include <numbers>
+#include <iterator> 
+#include <condition_variable> 
+#include <thread>         
+#include <mutex>  
+#include <cstdlib>
+#include <fstream>
+
+#include "program_properties.hpp";
+
+using namespace ProgramProperties;
+
+namespace ProgramProperties {
+
+
+}
