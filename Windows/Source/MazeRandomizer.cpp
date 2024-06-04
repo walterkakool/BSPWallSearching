7c04b8a9b812a9d806e4b47b8fd851e979cedfd6
walkk <72165225+walterkakool@users.noreply.github.com>
2024-06-04 13:28:28 -0700

    Letted it generate maze more random

--- Windows/Source/MazeRandomizer.cpp
+++ Windows/Source/MazeRandomizer.cpp
@@ -8,13 +8,15 @@
 #include <stdio.h>
 using namespace std;
 
+#define MAX_ITERATION 64
+
 int main()
 {
       ofstream fMaze ("maze.txt");
       srand( (unsigned int)time(NULL) );
       std::vector< std::complex<unsigned int> > givenPoints;  
 
-      for(int i = 0; i < 32; ++i) givenPoints.push_back( std::complex<unsigned int>( rand()%1020, rand()%1020 ) );
+      for(int i = 0; i < MAX_ITERATION; ++i) givenPoints.push_back( std::complex<unsigned int>( rand()%1020, rand()%1020 ) );
       fMaze<<"P1 1024 1024 ";
 
      std::complex<unsigned int> currentPoint(1,1); 
@@ -37,13 +39,18 @@ int main()
 
                 currentPoint = std::complex<unsigned int> ( rand()%1024, rand()%1024 );
 
-                    for(int k = 0; k < 32; ++k) {
+                    for(int k = 0; k < MAX_ITERATION; ++k) {
 
                         if( currentPoint.real() == givenPoints[k].real() || currentPoint.imag() == givenPoints[k].imag() ) {
                             
-                            fMaze<<"1";
-                            k = 32;
-                        }else if ( k == 31 ) fMaze<<"0"; 
+                            if( i == 1025 )
+                                fMaze<<"0";
+                            else
+                                fMaze<<"1";
+
+                            k = MAX_ITERATION;
+
+                        }else if ( k == MAX_ITERATION -1  ) fMaze<<"0"; 
 
                     }
                             
